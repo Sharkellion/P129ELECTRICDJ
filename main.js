e66386d1a06667c350decffa3a7d6f15166df7a1
Sharkellion <99950859+Sharkellion@users.noreply.github.com>
2022-05-14 10:22:07 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-song = "";
+song1 = "";
+song2 = "";
 
-function preload()
-{
-	song1 = loadSound("song(1).mp3");
-	song2=loadSound("song(2).mp3");
-}
+song1_status = "";
+song2_status = "";
 
 scoreRightWrist = 0;
 scoreLeftWrist = 0;
@@ -14,6 +12,12 @@ rightWristY = 0;
 
 leftWristX = 0;
 leftWristY = 0;
+
+function preload()
+{
+	song1 = loadSound("s1.mp3");
+	song2 = loadSound("s2.mp3");
+}
 
 function setup() {
 	canvas =  createCanvas(600, 500);
@@ -34,9 +38,10 @@ function gotPoses(results)
 {
   if(results.length > 0)
   {
+	console.log(results);
 	scoreRightWrist =  results[0].pose.keypoints[10].score;
 	scoreLeftWrist =  results[0].pose.keypoints[9].score;
-	console.log("scoreRightWrist = " + scoreRightWrist + " scoreLeftWrist = " + scoreLeftWrist);
+	console.log("scoreRightWrist = " + scoreRightWrist + "scoreLeftWrist = " + scoreLeftWrist);
 	
 	rightWristX = results[0].pose.rightWrist.x;
 	rightWristY = results[0].pose.rightWrist.y;
@@ -51,6 +56,9 @@ function gotPoses(results)
 
 function draw() {
 	image(video, 0, 0, 600, 500);
+	
+	song1_status = song1.isPlaying();
+	song2_status = song2.isPlaying();
 
 	fill("#FF0000");
 	stroke("#FF0000");
@@ -59,22 +67,26 @@ function draw() {
 	{ 
 		circle(rightWristX,rightWristY,20);
 
-		song2.stop();
-		if(song1_status==false){
-		    song1.play();
-		    document.getElementById("song").innerHTML="Playing-Shivers by Ed Sheeran";
-		    
+			song2.stop();
+
+		if(song1_status == false)
+		{
+			song1.play();
+			document.getElementById("song").innerHTML = "Playing - Harry Potter Theme Song"
+		}
 	}
 
-	if(scoreLeftWrist > 0.2
-	{ 
+	if(scoreLeftWrist > 0.2)
+	{
 		circle(leftWristX,leftWristY,20);
 
-		song1.stop();
-		if(song2_status==false){
-		    song2.play();
-		    document.getElementById("song").innerHTML="Playing- Dynamite by BTS";
-		    
+			song1.stop();
+
+		if(song2_status == false)
+		{
+			song2.play();
+			document.getElementById("song").innerHTML = "Playing - Peter Pan Song"
+		}
 	}
 
 }
@@ -85,5 +97,3 @@ function play()
 	song.setVolume(1);
 	song.rate(1);
 }
-
-
